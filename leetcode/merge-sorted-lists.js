45920e1e1a62af6feb18beb0c9ba2f706dfca12f
Erons <eronmmer@gmail.com>
2021-01-30 01:32:21 +0100

    merge sorted lists and valid parenthesis

--- leetcode/merge-sorted-lists.js
+++ leetcode/merge-sorted-lists.js
@@ -1,15 +1,36 @@
 // link to challenge -> https://leetcode.com/problems/merge-two-sorted-lists/
-// NOT DONE YETTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
-const list = {
-	val: 4,
-	next: {
-		val: 5,
-		next: {
-			val: 6,
-			next: {
-				val: 7,
-				next: null,
-			},
-		},
-	},
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+	let dummy = new ListNode(0),
+		current = dummy;
+	while (l1 && l2) {
+		if (l1.val <= l2.val) {
+			current.next = l1;
+			l1 = l1.next;
+		} else {
+			current.next = l2;
+			l2 = l2.next;
+		}
+		current = current.next;
+	}
+
+	if (l1) {
+		current.next = l1;
+	}
+	if (l2) {
+		current.next = l2;
+	}
+	return dummy.next;
 };
---
+++ leetcode/valid-parenthesis.js
@@ -0,0 +1,19 @@
+// Link to challenge -> https://leetcode.com/problems/valid-parentheses/submissions/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+	if (s.length === 0) return false;
+	const map = { "(": ")", "{": "}", "[": "]" };
+	const stack = [];
+	for (let char of s) {
+		if (map[char]) {
+			stack.push(char);
+		} else {
+			let last = stack.pop();
+			if (map[last] !== char) return false;
+		}
+	}
+	return stack.length === 0;
+};
