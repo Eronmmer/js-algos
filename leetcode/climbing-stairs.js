5687a8b2675f594ca21b039e9db931e12ef346cb
Erons <eronmmer@gmail.com>
2021-01-30 21:40:39 +0100

    climbing stairs

---
+++ leetcode/climbing-stairs.js
@@ -0,0 +1,17 @@
+// Link to challenge -> https://leetcode.com/problems/climbing-stairs/submissions/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+	const memo = new Map();
+	const climb = (x, y, memo) => {
+		if (x > y) return 0;
+		if (x === y) return 1;
+		if (memo.get(x) > 0) return memo.get(x);
+		memo.set(x, climb(x + 1, n, memo) + climb(x + 2, n, memo));
+		return memo.get(x);
+	};
+	return climb(0, n, memo);
+};
