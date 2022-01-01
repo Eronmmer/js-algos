42c2d842f8ee0d3617cc9797f7540787dc19038f
Erons <eronmmer@gmail.com>
2022-01-02 00:33:18 +0100

    update 2 sum & 3 sum

--- leetcode/3-sum.js
+++ leetcode/3-sum.js
@@ -3,8 +3,18 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-	nums.sort();
-	const res = [];
+	/* 
+    First and foremost, sort the array
+    Now, you can work with two nested loops. A `for` loop
+    and an inner while loop. 
+    The idea is to find two other elements that sum up with the current number
+    to give 0. So you'd need two pointers.
+    While doing this, just make sure you do some checks to meet the edge cases 
+    mentioned in the question. For this question, the edge case is that the solution set 
+    must not contain duplicate triplets.
+    */
+	nums.sort((a, b) => a - b);
+	let res = [];
 	for (let i = 0; i < nums.length - 2; i++) {
 		if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
 			let low = i + 1,
@@ -12,10 +22,9 @@ var threeSum = function (nums) {
 				sum = 0 - nums[i];
 			while (low < high) {
 				if (nums[low] + nums[high] === sum) {
-					let arr = [nums[i], nums[low], nums[high]];
-					res.push(arr);
-					while (low < high && nums[low] == nums[low + 1]) low++;
-					while (low < high && nums[high] == nums[high - 1]) high--;
+					res.push([nums[i], nums[low], nums[high]]);
+					while (low < high && nums[low] === nums[low + 1]) low++;
+					while (low < high && nums[high] == nums[low - 1]) high--;
 					low++;
 					high--;
 				} else if (nums[low] + nums[high] > sum) {
--- leetcode/two-sum.js
+++ leetcode/two-sum.js
@@ -1,13 +1,14 @@
 // link to challenge => https://leetcode.com/problems/two-sum/
 
-const twoSum = function (nums, target) {
-	let arr =
-		nums.findIndex((e) => e < 0) > -1 ? nums : nums.filter((e) => e <= target);
-	for (let i = arr.length - 1; i >= 0; i--) {
-		let modified = [...arr];
-		modified.splice(i, 1);
-		let second = modified.find((e) => e + arr[i] === target);
-		if (second != undefined)
-			return [nums.indexOf(arr[i]), nums.lastIndexOf(second)];
+var twoSum = function (nums, target) {
+	let differences = {};
+	for (let i = 0; i < nums.length; i++) {
+		let currValue = nums[i],
+			differenceIdx = differences[currValue];
+		if (differenceIdx !== undefined) {
+			return [differenceIdx, i];
+		} else {
+			differences[target - currValue] = i;
+		}
 	}
 };
